4adfa673c2a51a6ddb6cac784f5ead84fdfe5161
Sagar Goel <sagar.goel1911@gmail.com>
2023-12-26 21:57:00 +0530

    First Project added

---
+++ api-caller/src/App.js
@@ -0,0 +1,12 @@
+import InputApiCaller from './components/InputApiCaller';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <InputApiCaller/>
+    </div>
+  );
+}
+
+export default App;
