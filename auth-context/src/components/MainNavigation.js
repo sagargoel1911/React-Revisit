06868184a743b7893fe5b60f64e94a2675a246ed
Sagar Goel <sagar.goel1911@gmail.com>
2023-12-29 11:11:38 +0530

    Add useReducer, preventDefault, input changeHandler, input touchHandler, input validation, button disabling if input not valid and custom message for not valid input

--- auth-context/src/components/MainNavigation.js
+++ auth-context/src/components/MainNavigation.js
@@ -1,15 +1,50 @@
-import React, { useContext, useState } from "react";
+import React, { useContext, useReducer } from "react";
 import { NavLink } from "react-router-dom";
 import { AuthContext } from "../context/AuthContext";
 
+function inputReducer(state,action){
+  switch(action.type){
+    case "TOUCH": {
+      return {
+        ...state,
+        isTouched: true
+      };
+    }
+    case "CHANGE":{
+      return {
+        ...state,
+        value: action.value,
+        isValid:action.value.trim().length>0
+      }
+    }
+    default: return state; 
+  }
+}
+
 function MainNavigation() {
-  const [name, setName] = useState("");
-  function clickHandler() {
+  const [inputState, dispatch] = useReducer(inputReducer,{
+    value:"",
+    isTouched: false,
+    isValid: false
+  });
+  function clickHandler(event) {
+    event.preventDefault();
     if (auth.userName) {
       auth.logout();
     } else {
-      auth.login(name);
+      auth.login(inputState.value);
     }
+  }
+  function touchHandler(){
+    dispatch({
+      type:"TOUCH",
+    })
+  }
+  function changeHandler(event){
+    dispatch({
+      type:"CHANGE",
+      value:event.target.value
+    })
   }
   const auth = useContext(AuthContext);
   return (
@@ -27,12 +62,13 @@ function MainNavigation() {
         </li>
       )}
       <li>
-        {!auth.isLoggedIn && <input value={name} onChange={(event) => setName(event.target.value)} />}
+        {!auth.isLoggedIn && <input value={inputState.value} onChange={changeHandler} onBlur={touchHandler} />}
         <NavLink to="/">
-          <button onClick={clickHandler}>
+          <button onClick={clickHandler} disabled={!inputState.isValid && !auth.isLoggedIn}>
             {auth.isLoggedIn ? "Logout" : "Login"}
           </button>
         </NavLink>
+        {!auth.isLoggedIn && inputState.isTouched && !inputState.isValid && <>Please enter something</>}
       </li>
       <li>
         <NavLink to="/">
